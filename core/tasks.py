d4f747357ee54687e9acb59fb427bac279b54fb5
Aswitha kannoju <aswithakannoju17@gmail.com>
2026-04-05 14:58:45 +0530

    Add SimpleTask class with state and actions

--- core/tasks.py
+++ core/tasks.py
@@ -1,1 +1,11 @@
+from core.models import State, Action
 
+class SimpleTask:
+    def get_initial_state(self) -> State:
+        return State(data={"step": 0})
+
+    def get_possible_actions(self, state: State):
+        return [
+            Action(name="move_forward", params={}),
+            Action(name="wait", params={})
+        ]
